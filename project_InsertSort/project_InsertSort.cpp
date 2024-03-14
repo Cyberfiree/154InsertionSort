22334fed143be3e8553d89b03354825047daeee4
Cyberfire <buayagoreng99@gmail.com>
2024-03-14 14:02:17 +0800

    Membuat Prosedure display

--- project_InsertSort/project_InsertSort.cpp
+++ project_InsertSort/project_InsertSort.cpp
@@ -56,6 +56,18 @@ void insetionsort() {
 		}
 	}
 }
+
+void display() {
+	cout << endl;
+	cout << "\n========================" << endl;
+	cout << "Element Array yang disusun" << endl;
+	cout << "==========================" << endl;
+
+	for (int j = 0; j < n; j++) {
+		cout << arr[j] << endl;
+	}
+	cout << endl;
+}
 int main()
 {
 
