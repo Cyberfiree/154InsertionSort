32c02a82d61a60ca6faa1d19ca2daaf1d78120dd
Cyberfire <buayagoreng99@gmail.com>
2024-03-14 14:23:12 +0800

    Membuat keterangan masing masing function

--- project_InsertSort/project_InsertSort.cpp
+++ project_InsertSort/project_InsertSort.cpp
@@ -1,77 +1,77 @@
 #include <iostream>
 using namespace std;
 
-int arr[20];
-int n;
+int arr[20];																			//Mmebuat Array dengan panjang data 20
+int n;																					//Membuat variabel input n
 
-void input() {
+void input() {																			//Procedure Input
 	while (true)
 	{
-		cout << "Masukkan Jumlah Data pada Array : ";
-		cin >> n;
+		cout << "Masukkan Jumlah Data pada Array : ";									//Membuat Inputan jumlah element Array
+		cin >> n;																		//Mmemanggil variabel inputan n
 
-		if (n <= 20) {
+		if (n <= 20) {																	//Mmebuat kondisi n tidak lebih dari 20
 			break;
 		}
 		else
 		{
-			cout << "\nArray yang anda masukkan maksimal 20 element.\n";
+			cout << "\nArray yang anda masukkan maksimal 20 element.\n";				//Menampilkan pesan jika data lebih dari 20
 		}
 	}
-	cout << endl;
-	cout << "======================" << endl;
+	cout << endl;																		//Membuat jarak per baris program
+	cout << "======================" << endl;											//Membuat tampilan susunan data element array
 	cout << "Masukkan Element Array" << endl;
 	cout << "======================" << endl; 
 
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < n; i++)															//Menggunakan perulangan for untuk menyimpan data pada array
 	{
-		cout << "Data Ke-" << (i + 1) << ": ";
-		cin >> arr[1];
+		cout << "Data Ke-" << (i + 1) << ": ";											//Masukkan atau menginputkan nilai data n
+		cin >> arr[1];																	//Menyimpan nilai data n kedalam arr
 	}
 }
 
-void insertionsort() {
+void insertionsort() {																	//Procedure InsertionSort
 
-	int temp;
-	int i, j;
+	int temp;																			//Membuat variabel data temporer atau menyimopan sementara
+	int i, j;																			//Mmebuat variabel j sebagai penanda
 
-	for (i = 1; 1 <= n - 1; i++) {
+	for (i = 1; 1 <= n - 1; i++) {														//1.Looping dengan nilai i dimulai dari 1 hingga n-1
 
-		temp = arr[i];
+		temp = arr[i];																	//2.Simpan nilai arr[i] ke variabel sementara temp
 
-		j = i - 1;
+		j = i - 1;																		//3.Setting nilai j sama dengan i-1;
 
-		while (j >= 0 && arr[j] > temp)
-
+		while (j >= 0 && arr[j] > temp)													//4.Looping while dimana nilai j lebih besar sama dengan 0 dan
+																						//arr[j] lebih besar daripada temp
 		{
-			arr[j + 1] = arr[j];
-			j--;
+			arr[j + 1] = arr[j];														//4a.Simpan arr[j] ke dalam variabel arr[j + 1]
+			j--;																		//4b.Decreement nilai j by 1
 		}
 
-		arr[j + 1] = temp;
+		arr[j + 1] = temp;																//5.Simpan nilai temp ke dalam arr[j + 1}
 
-		cout << "\nPass" << i << ": ";
-		for (int k = 0; k < n; k++) {
-			cout << arr[k] << ": ";
+		cout << "\nPass" << i << ": ";													//Output ke layar
+		for (int k = 0; k < n; k++) {													//Looping nilai k dimulai dari 0 hingga n-1
+			cout << arr[k] << ": ";														//Output ke layar
 		}
 	}
 }
 
-void display() {
-	cout << endl;
-	cout << "\n========================" << endl;
-	cout << "Element Array yang disusun" << endl;
-	cout << "==========================" << endl;
+void display() {																		//Procedure Display
+	cout << endl;																		//Output baris kosong
+	cout << "\n========================" << endl;										//Output ke layar
+	cout << "Element Array yang disusun" << endl;										//Output ke layar
+	cout << "==========================" << endl;										//Output ke layar
 
-	for (int j = 0; j < n; j++) {
-		cout << arr[j] << endl;
+	for (int j = 0; j < n; j++) {														//Looping dengan j dimulai dari 0 hingga n-1
+		cout << arr[j] << endl;															//Output ke layar
 	}
-	cout << endl;
+	cout << endl;																		//Output baris kosong
 }
 
 int main()
 {
-	input();
-	insertionsort();
-	display();
+	input();																			//Memanggil input
+	insertionsort();																	//Memanggil insertionsort
+	display();																			//Memanggil display
 }
