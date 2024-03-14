95660f0f2243a384dd0e9b2208951554f4dad9f2
Cyberfire <buayagoreng99@gmail.com>
2024-03-14 13:43:39 +0800

    membuat prosedure input

--- project_InsertSort/project_InsertSort.cpp
+++ project_InsertSort/project_InsertSort.cpp
@@ -4,6 +4,32 @@ using namespace std;
 int arr[20];
 int n;
 
+void input() {
+	while (true)
+	{
+		cout << "Masukkan Jumlah Data pada Array : ";
+		cin >> n;
+
+		if (n <= 20) {
+			break;
+		}
+		else
+		{
+			cout << "\nArray yang anda masukkan maksimal 20 element.\n";
+		}
+	}
+	cout << endl;
+	cout << "======================" << endl;
+	cout << "Masukkan Element Array" << endl;
+	cout << "======================" << endl; 
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "Data Ke-" << (i + 1) << ": ";
+		cin >> arr[1];
+	}
+}
+
 int main()
 {
 
