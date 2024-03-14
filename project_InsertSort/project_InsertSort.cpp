926002cb70cb8e30ebeeb4c79d86735241097589
Cyberfire <buayagoreng99@gmail.com>
2024-03-14 13:48:55 +0800

    membuat procedure insertionsort

--- project_InsertSort/project_InsertSort.cpp
+++ project_InsertSort/project_InsertSort.cpp
@@ -30,6 +30,32 @@ void input() {
 	}
 }
 
+void insetionsort() {
+
+	int temp;
+	int i, j;
+
+	for (i = 1; 1 <= n - 1; i++) {
+
+		temp = arr[i];
+
+		j = i - 1;
+
+		while (j >= 0 && arr[j] > temp)
+
+		{
+			arr[j + 1] = arr[j];
+			j--;
+		}
+
+		arr[j + 1] = temp;
+
+		cout << "\nPass" << i << ": ";
+		for (int k = 0; k < n; k++) {
+			cout << arr[k] << ": ";
+		}
+	}
+}
 int main()
 {
 
